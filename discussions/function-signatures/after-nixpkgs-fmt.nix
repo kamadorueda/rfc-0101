5b0335fc3a36695a5fec0d87319ccc7373d4066d
Kevin Amado <kamadorueda@gmail.com>
2022-03-09 13:34:36 -0500

    feat: more function signatures

--- discussions/function-signatures/after-nixpkgs-fmt.nix
+++ discussions/function-signatures/after-nixpkgs-fmt.nix
@@ -1,1 +1,28 @@
-{ foo, bar, baz }: foo
+[
+  # Short that was single-line
+  ({ nixpkgs, ... }@attrs: nixpkgs)
+
+  # Short that was multi-line
+  ({ nixpkgs
+   , ...
+   }@attrs: nixpkgs)
+
+  # Long that was single-line
+  (attrs@{ stdenv, lib, gcc, coreutils, gnufind, gnugrep, gnumake, gnutar, gnupg, gnused, gnuk, gnubg, ... }: stdenv)
+
+  # Long that was multi-line
+  (attrs@{ stdenv
+   , lib
+   , gcc
+   , coreutils
+   , gnufind
+   , gnugrep
+   , gnumake
+   , gnutar
+   , gnupg
+   , gnused
+   , gnuk
+   , gnubg
+   , ...
+   }: stdenv)
+]
