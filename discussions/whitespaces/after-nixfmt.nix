6adae4fe5ba200d7307a1b40804d52e02884ea11
Kevin Amado <kamadorueda@gmail.com>
2022-03-09 13:47:07 -0500

    feat: whitespace discussion

---
+++ discussions/whitespaces/after-nixfmt.nix
@@ -0,0 +1,23 @@
+[
+  # Whitespace in lists
+  [ ]
+  [ 1 ]
+  [
+    1
+    2
+  ]
+
+  # Whitespace in attribute sets
+  { }
+  { foo = bar; }
+  {
+    foo = bar;
+    bar = baz;
+  }
+
+  # Whitespace in function signatures
+  (a@{ }: _)
+  ({ }@a: _)
+  ({ a }@b: _)
+  ({ a }@b: _)
+]
