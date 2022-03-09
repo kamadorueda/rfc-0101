bf5ed2d6c60dcd5549fdd4002a585bceb16a38af
Kevin Amado <kamadorueda@gmail.com>
2022-03-09 14:21:17 -0500

    feat: if-then-else discussion

---
+++ discussions/if-then-else/after-alejandra.nix
@@ -0,0 +1,20 @@
+[
+  # small
+  (
+    if cond
+    then foo
+    else bar
+  )
+
+  # nested
+  (
+    if cond
+    then
+      if looooooooooooooooooooooooooooooooooooooooooooooooooooong
+      then foo
+      else bar
+    else if cond
+    then foo
+    else bar
+  )
+]
